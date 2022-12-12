c73b4ba1aa27f5d287885ec79cecc761e31f3035
Vinayj1548 <113287840+Vinayj1548@users.noreply.github.com>
2022-12-12 14:02:58 +0530

    Updates

---
+++ precache-manifest.843200b5e2e1ea60e3fb2f947c6b0f55.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "7a135866a8e788d69a27",
+    "url": "/iNotebook-React/static/css/main.eae9b873.chunk.css"
+  },
+  {
+    "revision": "7a135866a8e788d69a27",
+    "url": "/iNotebook-React/static/js/main.7a135866.chunk.js"
+  },
+  {
+    "revision": "1b3f48c6de62d66542ce",
+    "url": "/iNotebook-React/static/js/1.1b3f48c6.chunk.js"
+  },
+  {
+    "revision": "cb2bd676ad692ed768e5",
+    "url": "/iNotebook-React/static/js/2.cb2bd676.chunk.js"
+  },
+  {
+    "revision": "0decb955950cb2546d8f",
+    "url": "/iNotebook-React/static/js/runtime~main.0decb955.js"
+  },
+  {
+    "revision": "0350df6246aa6f5cc09b3969127fa23e",
+    "url": "/iNotebook-React/index.html"
+  }
+];
